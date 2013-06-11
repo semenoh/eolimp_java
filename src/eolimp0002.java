089a6ae7386a5291a1de2903ae5dd134e1028606
semenoh <semenoh@gmail.com>
2013-06-11 15:56:03 +0300

    Update eolimp0002.java

--- src/eolimp0002.java
+++ src/eolimp0002.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class eolimp_0002_digits {
+public class eolimp0002 {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		long n = in.nextLong();
