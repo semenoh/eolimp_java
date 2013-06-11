114cbe336ee1663acfce7437818a57d54b386d33
semenoh <semenoh@gmail.com>
2013-06-11 15:55:35 +0300

    Update eolimp0001.java

--- src/eolimp0001.java
+++ src/eolimp0001.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class eolimp0001 {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     PrintWriter out = new PrintWriter(System.out);
